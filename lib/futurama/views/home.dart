810895af70d9f91002ace9f87f0a8c5351f0a591
Johnson <johnson@Johnsons-MacBook-Pro.local>
2022-04-12 10:51:02 +0100

    Adding Screenshots

--- lib/futurama/views/home.dart
+++ lib/futurama/views/home.dart
@@ -6,10 +6,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 
-import '../../tab_bar.dart';
 import '../constants.dart';
 import '../widgets/info_page.dart';
-import '../widgets/info_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
